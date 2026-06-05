e318b85414a91fdfb64a6f30708e229ba01764bc
lucas <lucas@debian.debian>
2026-06-05 19:35:10 -0300

    módulo vlc corrigido

--- vlc_ns3_example/vlc/model/vlc-channel-model.h
+++ vlc_ns3_example/vlc/model/vlc-channel-model.h
@@ -1,118 +1,137 @@
 #ifndef VLC_CHANNEL_HELPER_H_
 #define VLC_CHANNEL_HELPER_H_
 
-#include "ns3/propagation-loss-model.h"
-#include "ns3/propagation-delay-model.h"
-#include "ns3/pointer.h"
-#include "ns3/ptr.h"
 #include "ns3/core-module.h"
-#include "ns3/mobility-module.h"
-#include "ns3/vlc-propagation-loss-model.h"
-#include "ns3/vlc-snr.h"
-#include "ns3/vlc-error-model.h"
 #include "ns3/log.h"
-#include "ns3/vlc-net-device.h"
-#include "ns3/vlc-tx-net-device.h"
-#include "ns3/vlc-rx-net-device.h"
+#include "ns3/mobility-module.h"
 #include "ns3/object.h"
 #include "ns3/packet.h"
+#include "ns3/pointer.h"
+#include "ns3/propagation-delay-model.h"
+#include "ns3/propagation-loss-model.h"
+#include "ns3/ptr.h"
 #include "ns3/traced-callback.h"
-#include <cmath>
+#include "ns3/vlc-error-model.h"
+#include "ns3/vlc-net-device.h"
+#include "ns3/vlc-propagation-loss-model.h"
+#include "ns3/vlc-rx-net-device.h"
+#include "ns3/vlc-snr.h"
+#include "ns3/vlc-tx-net-device.h"
+
 #include <algorithm>
+#include <cmath>
 
-namespace ns3 {
+namespace ns3
+{
 
-class VlcChannel: public Channel {
-public:
-	VlcChannel();
+class VlcChannel : public Channel
+{
+  public:
+    VlcChannel();
 
-	virtual ~VlcChannel();
+    virtual ~VlcChannel();
 
-	static TypeId GetTypeId(void);
+    static TypeId GetTypeId(void);
 
-	virtual uint32_t GetNDevices(void) const; //gets the number of devices attached to the channel.
+    virtual std::size_t GetNDevices(
+        void) const; // gets the number of devices attached to the channel.
 
-	virtual Ptr<NetDevice> GetDevice(uint32_t i) const; //gets the device specidied by the index number.
+    virtual Ptr<NetDevice> GetDevice(
+        std::size_t i) const; // gets the device specidied by the index number.
 
-	void SetPropagationLossModel(ns3::Ptr<ns3::PropagationLossModel> loss); //sets the propagation loss model for the channel
+    void SetPropagationLossModel(ns3::Ptr<ns3::PropagationLossModel>
+                                     loss); // sets the propagation loss model for the channel
 
-	ns3::Ptr<ns3::PropagationLossModel> GetPropagationLossModel();//returns a pointer to the propagation loss model
+    ns3::Ptr<ns3::PropagationLossModel> GetPropagationLossModel(); // returns a pointer to the
+                                                                   // propagation loss model
 
-	void SetPropagationDelayModel(ns3::Ptr<ns3::PropagationDelayModel> delay); //sets the propagation delay model for the channel
+    void SetPropagationDelayModel(ns3::Ptr<ns3::PropagationDelayModel>
+                                      delay); // sets the propagation delay model for the channel
 
-	ns3::Ptr<ns3::PropagationDelayModel> GetPropagationDelayModel(); //returns the propagation delay model of the channel
+    ns3::Ptr<ns3::PropagationDelayModel> GetPropagationDelayModel(); // returns the propagation
+                                                                     // delay model of the channel
 
-	void SetPropagationDelay(double delay); 
+    void SetPropagationDelay(double delay);
 
-	double GetDistance(); //returns the distance after computing the distance using the mobility models of TX and RX
+    double GetDistance(); // returns the distance after computing the distance using the mobility
+                          // models of TX and RX
 
-	void SetDistance();	//sets the distance after computing the distance using the mobility models of TX and RX
+    void SetDistance(); // sets the distance after computing the distance using the mobility models
+                        // of TX and RX
 
-	void DoCalcPropagationLoss();//calculates the optical power signal for the RX using the optical power signal of the TX and loss model
+    void DoCalcPropagationLoss(); // calculates the optical power signal for the RX using the
+                                  // optical power signal of the TX and loss model
 
-	double DoCalcPropagationLossForSignal(int timeInstant);	//computes the optical power signal of RX at a particular instant
+    double DoCalcPropagationLossForSignal(
+        int timeInstant); // computes the optical power signal of RX at a particular instant
 
-	void SetAveragePower(double power);	//computes the average power of the signals
+    void SetAveragePower(double power); // computes the average power of the signals
 
-	double GetAveragePower();		//returns the average power of the signals
+    double GetAveragePower(); // returns the average power of the signals
 
-	void SetWavelength(int lower, int upper);// sets upper and lower bound wavelength [nm]
+    void SetWavelength(int lower, int upper); // sets upper and lower bound wavelength [nm]
 
-	void SetTemperature(double t);	// sets the black body temperature of LED
+    void SetTemperature(double t); // sets the black body temperature of LED
 
-	double GetTemperature(); //returns the temperature value as double.
+    double GetTemperature(); // returns the temperature value as double.
 
-	void SetReceivedPower(double p);// sets the average received optical signal power
+    void SetReceivedPower(double p); // sets the average received optical signal power
 
-	void CalculateNoiseVar();	//calculates the noise variance
+    void CalculateNoiseVar(); // calculates the noise variance
 
-	void CalculateSNR();		// calculates the SNR value
+    void CalculateSNR(); // calculates the SNR value
 
-	double GetSNR() const;		// returns the signal-to-noise ratio (SNR)
+    double GetSNR() const; // returns the signal-to-noise ratio (SNR)
 
-	void SetElectricNoiseBandWidth(double b);	// sets the noise bandwidth
+    void SetElectricNoiseBandWidth(double b); // sets the noise bandwidth
 
-	double GetNoiseBandwidth();	//return the noise bandwidth
+    double GetNoiseBandwidth(); // return the noise bandwidth
 
-	bool TransmitStart(Ptr<Packet> p, Ptr<VlcNetDevice> src, Time txTime); //starts the transmission of packets.
+    bool TransmitStart(Ptr<Packet> p,
+                       Ptr<VlcNetDevice> src,
+                       Time txTime); // starts the transmission of packets.
 
-	double GetDistance(ns3::Ptr<ns3::MobilityModel> aTX,
-	ns3::Ptr<ns3::MobilityModel> bRX) const; //returns the distance between sender and receiver devices.
+    double GetDistance(ns3::Ptr<ns3::MobilityModel> aTX,
+                       ns3::Ptr<ns3::MobilityModel> bRX)
+        const; // returns the distance between sender and receiver devices.
 
-	void TransmitDataPacket(Ptr<Packet> p); //starts the transmission of data packets.
+    void TransmitDataPacket(Ptr<Packet> p); // starts the transmission of data packets.
 
-	void EnqueueDataPacketAfterCorruption(); 
+    void EnqueueDataPacketAfterCorruption();
 
-	void Attach(Ptr<VlcNetDevice> device); 
+    void Attach(Ptr<VlcNetDevice> device);
 
-private:
-	ns3::Ptr<ns3::PropagationLossModel> m_loss;
+  private:
+    ns3::Ptr<ns3::PropagationLossModel> m_loss;
 
-	double m_distanceBWTXandRX;
+    double m_distanceBWTXandRX;
 
-	ns3::Ptr<ns3::PropagationDelayModel> m_delay;
+    ns3::Ptr<ns3::PropagationDelayModel> m_delay;
 
-	double m_AvgPower;
+    double m_AvgPower;
 
-	ns3::Ptr<ns3::VlcSnr> m_SNR;
+    ns3::Ptr<ns3::VlcSnr> m_SNR;
 
-	uint32_t m_nDevices; //number of devices.
+    uint32_t m_nDevices; // number of devices.
 
-	TracedCallback<Ptr<const Packet>, Ptr<NetDevice>, Ptr<NetDevice>, Time, Time> m_txrxVlcChannel;
+    TracedCallback<Ptr<const Packet>, Ptr<NetDevice>, Ptr<NetDevice>, Time, Time> m_txrxVlcChannel;
 
-	class WirelessLink { //wireless link class for storing a sender and receiver vlc device.
-	public:
-		WirelessLink() :
-				m_src(0), m_dst(0) {
-		}
+    class WirelessLink
+    { // wireless link class for storing a sender and receiver vlc device.
+      public:
+        WirelessLink()
+            : m_src(0),
+              m_dst(0)
+        {
+        }
 
-		Ptr<VlcNetDevice> m_src;   //!< First NetDevice
-		Ptr<VlcNetDevice> m_dst;   //!< Second NetDevice
-	};
+        Ptr<VlcNetDevice> m_src; //!< First NetDevice
+        Ptr<VlcNetDevice> m_dst; //!< Second NetDevice
+    };
 
-	WirelessLink m_link[2];
+    WirelessLink m_link[2];
 };
 
-} /* namespace vlc */
+} // namespace ns3
 
 #endif /* SRC_VLCNEW_VLCCHANNEL_H_ */
